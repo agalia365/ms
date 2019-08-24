00a3fcc58d907d20fc2ddafcb7f91c93d057e602
git365zhou <leo_365@126.com>
2019-08-24 16:56:03 +0800

    init

---
+++ ms-weather-api/src/main/java/com/leo/weather/msweatherapi/MsWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.leo.weather.msweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWeatherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsWeatherApiApplication.class, args);
+    }
+
+}
