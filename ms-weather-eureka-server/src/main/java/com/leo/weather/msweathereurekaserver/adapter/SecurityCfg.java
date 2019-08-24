125376f49269cf35b274d20fe7f5756eaa70696c
git365zhou <leo_365@126.com>
2019-08-24 22:45:38 +0800

    weather report micro service

---
+++ ms-weather-eureka-server/src/main/java/com/leo/weather/msweathereurekaserver/adapter/SecurityCfg.java
@@ -0,0 +1,16 @@
+package com.leo.weather.msweathereurekaserver.adapter;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityCfg extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
